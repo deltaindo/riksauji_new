9e2d74a208cd0c439d3e1bcccafa66c426a26f81
deltaindo <deltaindogroup@gmail.com>
2026-01-15 16:01:12 +0700

    feat: add clients master data API

---
+++ src/app/api/masters/clients/route.ts
@@ -0,0 +1,24 @@
+import { createServerSupabaseClient } from '@/lib/supabase/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  try {
+    const supabase = await createServerSupabaseClient()
+
+    const { data, error } = await supabase
+      .from('clients')
+      .select('*')
+      .order('nama_client')
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 400 })
+    }
+
+    return NextResponse.json(data)
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    )
+  }
+}
